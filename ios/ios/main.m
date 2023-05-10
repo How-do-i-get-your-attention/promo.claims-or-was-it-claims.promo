cdf5ba49c2406bad6deecb79fd1651783b64627f
How-do-i-get-your-attention <129417986+How-do-i-get-your-attention@users.noreply.github.com>
2023-05-10 14:17:45 +0200

    bv

--- ios/ios/main.m
+++ ios/ios/main.m
@@ -7,12 +7,11 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
+#import "AppPrincipal.h"
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        return UIApplicationMain(argc, argv, NSStringFromClass([AppPrincipal class]), NSStringFromClass([AppDelegate class]));
+        
     }
-    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 }
