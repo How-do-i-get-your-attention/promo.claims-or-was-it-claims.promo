251fdf2a0435f88670536fbdd403ab7c8d481e84
How-do-i-get-your-attention <129417986+How-do-i-get-your-attention@users.noreply.github.com>
2023-05-14 18:57:14 +0200

    from mac

---
+++ Software/from mac.cpp
@@ -0,0 +1,8 @@
+//
+//  from mac.cpp
+//  
+//
+//  Created by HOW TO GET YOUR ATTENSION on 14/05/2023.
+//
+
+#include "from mac.hpp"
---
+++ Software/from mac.hpp
@@ -0,0 +1,13 @@
+//
+//  from mac.hpp
+//  
+//
+//  Created by HOW TO GET YOUR ATTENSION on 14/05/2023.
+//
+
+#ifndef from_mac_hpp
+#define from_mac_hpp
+
+#include <stdio.h>
+
+#endif /* from_mac_hpp */
